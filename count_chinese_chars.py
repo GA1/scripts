4b935375c2fd5e0da9be8a4c89b3432be0d27918
GA1 <nvkga1@gmail.com>
2018-01-19 12:58:04 +0100

    Sort chars

--- count_chinese_chars.py
+++ count_chinese_chars.py
@@ -4,5 +4,7 @@ with open('chinese_characters.txt', 'r') as myfile:
   for i in range(len(s)):
     if s[i] > u'\u4e000' and s[i] < u'\u9fff':
       chars.add(s[i])
+  chars = list(chars)
+  chars.sort()
   print(chars)
   print('number of unique chinese characters is: ' + str(len(chars)))
