176862b949347c4537c56460b79529713509649e
GA1 <nvkga1@gmail.com>
2018-01-30 12:38:07 +0100

    Clean

--- audio_trimmer/audio_trimmer.py
+++ audio_trimmer/audio_trimmer.py
@@ -1,31 +1,38 @@
 from pydub import AudioSegment
 
 
-def trim(minuteStart, secondStart, input_file_path, output_file_path, minuteStop, secondStop):
+def trim(minute_start, second_start, minute_stop, second_stop, input_file_path, output_file_path):
     sound = AudioSegment.from_file(input_file_path, format="mp3")
-    trimmed_sound = sound[(minuteStart * 60 + secondStart) * 1000: (minuteStop * 60 + secondStop) * 1000]
+    trimmed_sound = sound[(minute_start * 60 + second_start) * 1000: (minute_stop * 60 + second_stop) * 1000]
     trimmed_sound.export(output_file_path, format="mp3")
 
 
 data = [
     [5, 20, 7, 27, "01"],
-    # [5, 20, 7, 27, "02"],
-    # [5, 20, 7, 27, "03"],
-    # [5, 20, 7, 27, "04"],
-    # [5, 20, 7, 27, "05"],
-    # [5, 20, 7, 27, "06"],
-    # [5, 20, 7, 27, "07"],
-    # [5, 20, 7, 27, "08"],
-    # [5, 20, 7, 27, "09"],
-    # [5, 20, 7, 27, "10"],
-    # [5, 20, 7, 27, "11"],
-    # [5, 20, 7, 27, "12"],
-    # [5, 20, 7, 27, "13"],
-    # [5, 20, 7, 27, "14"],
-    # [5, 20, 7, 27, "15"],
-    # [5, 20, 7, 27, "16"],
-    # [5, 20, 7, 27, "17"],
+    [5, 13, 7, 30, "02"],
+    [4, 20, 5, 55, "03"],
+    [4, 29, 6, 25, "04"],
+    [3, 43, 5, 41, "05"],
+    [5, 1, 6, 55, "06"],
+    [3, 43, 6, 26, "07"],
+    [2, 50, 4, 4, "08"],
+    [3, 28, 4, 53, "09"],
+    [3, 35, 4, 51, "10"],
+    [3, 49 , 5, 13, "11"],
+    [3, 51, 5, 32, "12"],
+    [2, 59, 4, 14, "13"],
+    [3, 41, 5, 4, "14"],
+    [5, 18, 7, 15, "15"],
+    [5, 42, 7, 27, "16"],
+    [7, 26, 9, 52, "16"],
 ]
 
 for d in data:
-    trim(d[0], d[1], d[2], 'input_files/' + [3] + '.mp3', 'results/' + [3] + '.mp3', d[4])
+    print(data[4])
+    trim(d[0],
+         d[1],
+         d[2],
+         d[3],
+         'input_files/' + d[4] + '.mp3',
+         'results/' + d[4] + '.mp3'
+         )
