3c1e3ec9dcd11178a1ef2eaa8962cd33106811e7
GA1 <nvkga1@gmail.com>
2018-03-22 19:56:19 +0100

    Update Chinese characters

--- open_links_in_browser/link_in_browser_opener.py
+++ open_links_in_browser/link_in_browser_opener.py
@@ -11,6 +11,8 @@ links_separated_by_new_lines = re.sub(r'\n+', '\n', links_separated_by_new_lines
 
 urls = links_separated_by_new_lines.split('\n')
 
-chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+# browser_path = 'open -a /Applications/Google\ Chrome.app %s'
+browser_path = 'open -a /Applications/Firefox.app %s'
 for u in urls:
-    webbrowser.get(chrome_path).open(u)
+    print(u)
+    webbrowser.get(browser_path).open_new_tab(u)
