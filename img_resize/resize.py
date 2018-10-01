ca6cd789eb5585177f6f65c3121005fe84a7632b
GA1 <nvkga1@gmail.com>
2018-10-01 20:20:44 +0100

    fix typo

--- img_resize/resize.py
+++ img_resize/resize.py
@@ -4,7 +4,7 @@ from PIL import Image
 from math import sqrt
 
 NUMBER_OF_MEGA_PIXELS = 4
-NUMNBER_OF_PIXELS_ON_RESIZED_IMAGE = 1024 * 1024 * NUMBER_OF_MEGA_PIXELS
+NUMBER_OF_PIXELS_ON_RESIZED_IMAGE = 1024 * 1024 * NUMBER_OF_MEGA_PIXELS
 image_extensions = ['.png', '.jpg', 'jpeg', '.bmp', ]
 input_directory = "./input/"
 output_directory = "./output/"
@@ -22,7 +22,7 @@ def resize(img):
     original_width = img.size[0]
     original_height = img.size[1]
     number_of_pixels_before_resizing = original_width * original_height
-    factor_of_resizing = number_of_pixels_before_resizing / NUMNBER_OF_PIXELS_ON_RESIZED_IMAGE
+    factor_of_resizing = number_of_pixels_before_resizing / NUMBER_OF_PIXELS_ON_RESIZED_IMAGE
     sqrt_of_factor_of_resizing = sqrt(factor_of_resizing)
     new_width = int(original_width / sqrt_of_factor_of_resizing)
     new_height = int(original_height / sqrt_of_factor_of_resizing)
