975bf7d9b2ea12abc5170f1a222fdac2c512fd6b
GA1 <nvkga1@gmail.com>
2020-04-13 16:34:36 +0200

    feat: add chinese song text merger

---
+++ chinese-song-parser/src/merge.js
@@ -0,0 +1,31 @@
+const {saveString} = require("./file-utils");
+const { getLines } = require('./file-utils')
+const simplifiedLines = getLines('./input/simplified.txt')
+const traditionalLines = getLines('./input/traditional.txt')
+const pinyinLines = getLines('./input/pinyin.txt')
+const englishLines = getLines('./input/english.txt')
+
+if (
+  simplifiedLines.length !== traditionalLines &&
+  traditionalLines.length !== pinyinLines.length &&
+  pinyinLines.length !== englishLines.length
+) {
+  console.log('Files have different number of lines')
+  console.log('simplified.txt: ' + simplifiedLines.length)
+  console.log('traditionalLines.txt: ' + traditionalLines.length)
+  console.log('pinyin.txt: ' + pinyinLines.length)
+  console.log('english.txt: ' + englishLines.length)
+} else {
+  const result = []
+  const N = simplifiedLines.length;
+  for (let i = 0; i < N; i++) {
+    result.push(simplifiedLines[i])
+    result.push(traditionalLines[i])
+    result.push('\n')
+    result.push(pinyinLines[i])
+    result.push(englishLines[i])
+    result.push('\n')
+    result.push('\n')
+  }
+  saveString(result.join('\n'), '../output/result.txt')
+}
