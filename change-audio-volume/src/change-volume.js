7f0e86bee1f5bd458c452d0f012a1e0c0d408f8c
GA1 <nvkga1@gmail.com>
2020-08-25 16:03:05 +0200

    feat: finish change volume script

---
+++ change-audio-volume/src/change-volume.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process');
+
+const VOLUME = 64.0
+
+const normalizeFileNames = () => {
+  const inputDir = path.join(__dirname, '..', 'input')
+
+  const outputDir = path.join(__dirname, '..', 'output', `volume-${VOLUME}`)
+  const inputFiles = fs.readdirSync(inputDir)
+  if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
+  }
+  for (let i = 0; i < inputFiles.length; i++) {
+    const fileName = inputFiles[i]
+    const command = `ffmpeg -i ${inputDir}/${fileName} -filter:a "volume=${VOLUME}" ${outputDir}/${fileName}`;
+    exec(`${command}`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+        // node couldn't execute the command
+        return;
+      }
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+    // ffmpeg -i input.wav -filter:a "volume=0.5" output.wav
+  }
+  // const outputFiles = fs.readdirSync(outputDir)
+}
+
+normalizeFileNames()
+
+exports.normalizeFileNames = normalizeFileNames
